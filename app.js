df3baa6f577d96305235c2236f68859bb9aa7f4f
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-01-28 22:28:54 +0530

    uses method like get and post

--- app.js
+++ app.js
@@ -10,6 +10,29 @@ app.listen(port,(err)=>{
     console.log(`Server is running on Port : ${port}`);
 });
 
-app.use((req,resp)=>{
-    console.log("request received!!");
-})
+// app.use((req,resp)=>{
+//     console.log("request received!!");
+//     // resp.send("This is Response from me");
+//     // resp.send({
+//     //     name:"sakshi",
+//     //     age:20
+//     // });
+//     let code = "<h1>SEND RESPONSE using send method</h1>";
+//     resp.send(code);
+// });
+
+app.get("/",(req,resp)=>{
+    resp.send("You are on ROOT path");
+});
+app.get("/about",(req,resp)=>{
+    resp.send("You are on About path");
+});
+app.get("/contact",(req,resp)=>{
+    resp.send("You are on ContactUS path");
+});
+app.get("*",(req,resp)=>{
+    resp.send("This path doesn't exit");
+});
+app.post("/",(req,resp)=>{
+    resp.send("We request POST  Method here");
+});
