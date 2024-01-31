a4388c8f4c49f602e8f9cdb10e9dd903de6e8269
Sakshi Chaurasia <121746813+chaurasiasakshi@users.noreply.github.com>
2024-01-31 07:35:47 +0530

    EJS Template

--- EJS Module/index..js
+++ EJS Module/index..js
@@ -1,8 +1,37 @@
 const express = require("express");
- const app = express();
+const app = express();
+const path = require("path");
+const port = 8080;
 
- const port = 8080;
+// View EJS  engine and static files
+app.set("view engine","ejs");
+// set path
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+});
+
+// Roll Dice 
+
+app.get("/rolldice",(req,res)=>{
+    let  diceVal = Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs", { diceVal });
+});
+app.get("/msg",(req,res)=>{
+    res.send("sending a msg");
+});
+
+// Instagram  EJS
+
+app.get("/ig/:username",(req,res)=>{
+    const follower = ["sak","sam","ria","ami","suku"]
+    let{ username } = req.params;
+    // console.log(username);
+    res.render("instagram.ejs", { username , follower});
+})
+
 
  app.listen(port,()=>{
     console.log(`server is listening on ${port}`)
- })
+ });
